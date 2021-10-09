44e86087b6ff6da8df2ad2b3218c0e28f67d909e
Abhishek Kumar <abhishek.kumar@mountblue.io>
2021-10-10 01:40:47 +0530

    add editorconfig prettier and eslint

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 export default function Welcome() {
-  return (
-    <h3 style={{ textAlign: "center", marginTop: 28 }}>
-      React kit welcome page component
-    </h3>
-  );
+	return (
+		<h3 style={{ textAlign: 'center', marginTop: 28 }}>
+			React kit welcome page component
+		</h3>
+	);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,51 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+/* eslint-disable */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-  template: path.join(__dirname, "examples/src/index.html"),
-  filename: "./index.html",
+	template: path.join(__dirname, 'examples/src/index.html'),
+	filename: './index.html',
 });
 
 module.exports = {
-  entry: path.join(__dirname, "examples/src/index.js"),
-  output: {
-    path: path.join(__dirname, "examples/dist"),
-    filename: "bundle.js",
-  },
-  plugins: [htmlWebpackPlugin],
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  devtool: "eval",
-  devServer: {
-    port: 6001,
-    hot: true,
-    open: true,
-    historyApiFallback: true,
-    client: {
-      overlay: true,
-      progress: true,
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        use: "babel-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(ico|gif|png|jpe?g|svg|)$/i,
-        loader: "file-loader",
-        options: {
-          name: "[path][name].[ext]",
-        },
-      },
-    ],
-  },
+	entry: path.join(__dirname, 'examples/src/index.js'),
+	output: {
+		path: path.join(__dirname, 'examples/dist'),
+		filename: 'bundle.js',
+	},
+	plugins: [htmlWebpackPlugin],
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		port: 6001,
+		hot: true,
+		open: true,
+		historyApiFallback: true,
+		client: {
+			overlay: true,
+			progress: true,
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/i,
+				use: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(ico|gif|png|jpe?g|svg|)$/i,
+				loader: 'file-loader',
+				options: {
+					name: '[path][name].[ext]',
+				},
+			},
+		],
+	},
 };
